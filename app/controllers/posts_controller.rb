39c3ec44f8f64585f5aaad076064a48378e627af
kylemurray25 <kylemurray@mail.adelphi.edu>
2016-05-11 05:12:05 +0000

    getting there

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,104 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  # GET /posts
+  # GET /posts.json
+  def index
+    @posts = Post.all
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+    tag_titles = @post.tags.map {|tag| tag.title }
+    @post.tag_titles = tag_titles.join(',')
+  end
+
+  # POST /posts
+  # POST /posts.json
+  def create
+    @post = current_user.posts.new(post_params)
+tags = get_tags(post_params[:tag_titles], ',')
+    tags.each do |tag|
+      @post.tags << tag
+    end
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+     tags = get_tags(post_params[:tag_titles], ',')
+    @post.tags.clear
+    tags.each do |tag|
+      @post.tags << tag
+    end
+    return head(:forbidden) unless @post.user == current_user
+    respond_to do |format|
+      
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /posts/1
+  # DELETE /posts/1.json
+  def destroy
+    @post.destroy
+    return head(:forbidden) unless @post.user == current_user
+    respond_to do |format|
+      
+      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+      def get_tags(str, delim)
+      titles = str.split(delim)
+      tags = []
+      titles.each do |title|
+        # GET /tag/:id/posts - this comment helps you to remember which URL will map to this method
+  def by_tag
+    @tag = Tag.find(params[:id]) # remember, find will raise an error if the record is not found
+    @posts = @tag.posts # get a list of all that tag's posts
+  end
+        title.strip!
+        next unless title && title.length # we don't want to add blank tags
+        tags << Tag.where(title: title).first_or_create
+      end
+      return tags
+    end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :body, :username, :tag_titles)
+    end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+    belongs_to :user
+    validates :user, presence: true
+    validates :title, presence: true
+    validates :body, presence: true
+    has_many :categorizations
+    has_many :tags, through: :categorizations
+    attr_accessor :tag_titles
+end
